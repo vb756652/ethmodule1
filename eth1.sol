3ba9ee63dd44431fe5fd26f8bd015e0e5160b2bd
Vimlesh  Kumar Pal <111525169+vb756652@users.noreply.github.com>
2023-08-18 22:04:20 +0530

    Update eth1.sol

--- eth1.sol
+++ eth1.sol
@@ -1,47 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.17;
 
-/*
-       REQUIREMENTS
-    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-    2. Your contract will have a mapping of addresses to balances (address => uint)
-    3. You will have a mint function that takes two parameters: an address and a value. 
-       The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
-    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the “sender”.
-    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
-       to the amount that is supposed to be burned.
-*/
+contract FileHandling{
+   
+    // uint public num = 0;
+    uint b=5;
 
-
-contract MyToken {
-
-    // public variables here
-    string public tokenName = "EHCP";
-    string public abbrv = "KLP";
-    uint public totalSupply = 0;
-    
-
-    // mapping variable here
-    mapping (address=> uint) public balances;
-
-    // mint function
-    function mint(address _address, uint _value) public {
-        totalSupply += _value;
-        balances[_address] += _value;  
-
+    function testAssert(uint num) public pure{
+        assert(num!=0);
     }
 
-
-    // burn function
-    function burn( address _address,uint _value) public {
-        if( balances[_address] >= _value){
-            totalSupply -= _value;
-            balances[_address] -= _value;
+    function divide(uint _numerator, uint _denomenator) public pure returns (uint){
+        if(_numerator<_denomenator){
+           
+            revert("please provide numerator greater than denomenator");
+            
         }
-        
+        return _numerator/_denomenator;
+       
+
+    }
+    function mult(uint a) public view returns (uint){
+        require(a>0,"Value of a is zero , we don't want the result to be zero");
+        return a*b;
+
     }
 
 }
