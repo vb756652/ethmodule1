e73aed86f4ea3465bb88821d222c7907a248bae8
Vimlesh  Kumar Pal <111525169+vb756652@users.noreply.github.com>
2023-08-20 21:47:54 +0530

    Update eth1.sol

--- eth1.sol
+++ eth1.sol
@@ -1,47 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.17;
 
-/*
-       REQUIREMENTS
-    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-    2. Your contract will have a mapping of addresses to balances (address => uint)
-    3. You will have a mint function that takes two parameters: an address and a value. 
-       The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
-    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the “sender”.
-    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
-       to the amount that is supposed to be burned.
-*/
+contract UpdatedErrorHandling {
 
+    uint public constant constantValue = 5;
 
-contract MyToken {
-
-    // public variables here
-    string public tokenName = "EHCP";
-    string public abbrv = "KLP";
-    uint public totalSupply = 0;
-    
-
-    // mapping variable here
-    mapping (address=> uint) public balances;
-
-    // mint function
-    function mint(address _address, uint _value) public {
-        totalSupply += _value;
-        balances[_address] += _value;  
-
+    function checkNonZero(uint num) public pure {
+        require(num != 0, "Input value must be non-zero");
     }
 
-
-    // burn function
-    function burn( address _address,uint _value) public {
-        if( balances[_address] >= _value){
-            totalSupply -= _value;
-            balances[_address] -= _value;
-        }
-        
+    function safeDivision(uint _numerator, uint _denominator) public pure returns (uint) {
+        require(_denominator != 0, "Denominator cannot be zero");
+        require(_numerator >= _denominator, "Numerator must be greater than or equal to denominator");
+        uint result = _numerator / _denominator;
+        return result;
     }
 
+    function multiplyWithConstant(uint a) public view returns (uint) {
+        require(a > 0, "Input value must be greater than zero");
+        uint result = a * constantValue;
+        return result;
+    }
+
+    function updateConstantValue(uint newValue) public {
+        require(newValue > 0, "New constant value must be greater than zero");
+        constantValue = newValue;
+    }
 }
